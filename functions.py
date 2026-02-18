cfd4d8089ddf89922e46775b4c49cecbb5f63ba6
Rajamadhuamathi-python <rajamadhumathi189396@gmail.com>
2026-02-19 02:00:04 +0800

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,12 @@
+# functions.py
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to Python.")
+
+greet("Raj")
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
